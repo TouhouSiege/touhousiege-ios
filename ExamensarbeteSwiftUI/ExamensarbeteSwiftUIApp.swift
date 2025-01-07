ae86e8f055a4b92541489cd3b496c1096cf332a3
MichihideS <michihides@gmail.com>
2025-01-07 10:35:58 +0100

    Initial Commit

---
+++ ExamensarbeteSwiftUI/ExamensarbeteSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExamensarbeteSwiftUIApp.swift
+//  ExamensarbeteSwiftUI
+//
+//  Created by Michihide Sugito on 2025-01-07.
+//
+
+import SwiftUI
+
+@main
+struct ExamensarbeteSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
