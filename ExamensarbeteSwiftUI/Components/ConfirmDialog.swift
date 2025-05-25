b66f6817b8c043c80599a7da69dfe46e1a478053
MichihideS <michihides@gmail.com>
2025-05-25 17:45:26 +0200

    add confirm dialog component for and add all elements from the shop to confirm

---
+++ ExamensarbeteSwiftUI/Components/ConfirmDialog.swift
@@ -0,0 +1,60 @@
+//
+//  ConfirmDialog.swift
+//  ExamensarbeteSwiftUI
+//
+//  Created by Michihide Sugito on 2025-05-25.
+//
+
+import SwiftUI
+
+struct ConfirmDialog: View {
+    let width: CGFloat = UIScreen.main.bounds.width
+    let height: CGFloat = UIScreen.main.bounds.height
+    
+    var functionYes: () -> Void
+    var functionNo: () -> Void
+    var title: String
+    var buyTitle: String
+    var buyText: String
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                Text(title)
+                    .font(TouhouSiegeStyle.FontSize.large.bold())
+                    .foregroundStyle(Color.generalBrown)
+                    .offset(y: width * TouhouSiegeStyle.Decimals.xSmall)
+                
+                Spacer()
+                
+                Text(buyTitle)
+                    .font(TouhouSiegeStyle.FontSize.small.bold())
+                    .foregroundStyle(Color.generalBrown)
+                
+                Text(buyText)
+                    .font(TouhouSiegeStyle.FontSize.xSmall)
+                    .foregroundStyle(Color.generalBrown)
+                
+                Spacer()
+                
+                HStack {
+                    ButtonSmall(function: {
+                        functionYes()
+                    }, text: "Yes")
+                    
+                    ButtonSmall(function: {
+                        functionNo()
+                    }, text: "No")
+                }
+                .offset(y: -width * TouhouSiegeStyle.Decimals.xSmall)
+            }
+            .frame(maxWidth: width * TouhouSiegeStyle.BigDecimals.medium, maxHeight: height * TouhouSiegeStyle.BigDecimals.medium)
+            .background {
+                RoundedRectangle(cornerRadius: TouhouSiegeStyle.CornerRadius.large)
+                    .fill(.thinMaterial.opacity(TouhouSiegeStyle.BigDecimals.xxxLarge))
+                    .stroke(.generalBrown, lineWidth: TouhouSiegeStyle.StrokeWidth.xSmall)
+            }
+        }
+    }
+}
+
--- ExamensarbeteSwiftUI/Screens/Authenticated/Shop/ShopView.swift
+++ ExamensarbeteSwiftUI/Screens/Authenticated/Shop/ShopView.swift
@@ -15,69 +15,131 @@ struct ShopView: View {
     let width: CGFloat = UIScreen.main.bounds.width
     let height: CGFloat = UIScreen.main.bounds.height
     
+    @State var buyTitle: String = ""
+    @State var buyText: String = ""
     @State var whichTab: Int = 1
+    @State var whichItem: Int = 0
+    @State var confirmDialog: Bool = false
+    @State var isAnimating = false
     
     var body: some View {
         ZStack {
-            BackgroundMain()
-            TopNavBar()
-            
-            VStack {
-                HStack {
-                    VStack {
-                        ButtonMedium(function: {
-                            whichTab = 1
-                            }, text: "Diamond")
-                        .disabled(whichTab == 1)
-                        .opacity(whichTab == 1 ? TouhouSiegeStyle.BigDecimals.Large : 1)
-                        
-                        ButtonMedium(function: {
-                            whichTab = 2
-                        }, text: "Gold")
-                        .disabled(whichTab == 2)
-                        .opacity(whichTab == 2 ? TouhouSiegeStyle.BigDecimals.Large : 1)
-                    }
-                    
-                    if whichTab == 1 {
-                        ShopItemLister(function: {
-                            
-                        }, titel: "Diamond Pack 1", text: "400 Diamonds", cost: "6.99€")
-                        
-                        ShopItemLister(function: {
-                            
-                        }, titel: "Diamond Pack 2", text: "900 Diamonds", cost: "12.99€")
-                        
-                        ShopItemLister(function: {
-                            
-                        }, titel: "Diamond Pack 3", text: "2000 Diamonds", cost: "24.99€")
-                    }
-                    
-                    if whichTab == 2 {
-                        ShopItemLister(function: {
-                            
-                        }, titel: "Stamina Pack", text: "120 Stamina", cost: "750", image: Image(TouhouSiegeStyle.Images.gold01))
-                            
-                        ShopItemLister(function: {
-                            
-                        }, titel: "Gold Pack 1", text: "400 Gold", cost: "100", image: Image(TouhouSiegeStyle.Images.diamond01))
-                        
-                        ShopItemLister(function: {
-                            
-                        }, titel: "Gold Pack 2", text: "1000 Gold", cost: "200", image: Image(TouhouSiegeStyle.Images.diamond01))
-                    }
-                }
-            }
-            
-            VStack {
-                Spacer()
+            ZStack {
+                BackgroundMain()
+                TopNavBar()
                 
-                HStack {
-                    ButtonBig(function: {
-                        navigationManager.navigateTo(screen: .home)
-                    }, text: "Back").offset(x: width * TouhouSiegeStyle.Decimals.medium, y: -width * TouhouSiegeStyle.Decimals.xSmall)
-                    
-                    Spacer()
+                VStack {
+                    HStack {
+                        VStack {
+                            ButtonMedium(function: {
+                                whichTab = 1
+                            }, text: "Diamond")
+                            .disabled(whichTab == 1)
+                            .opacity(whichTab == 1 ? TouhouSiegeStyle.BigDecimals.Large : 1)
+                            
+                            ButtonMedium(function: {
+                                whichTab = 2
+                            }, text: "Gold")
+                            .disabled(whichTab == 2)
+                            .opacity(whichTab == 2 ? TouhouSiegeStyle.BigDecimals.Large : 1)
+                        }
+                        
+                        if whichTab == 1 {
+                            ShopItemLister(function: {
+                                whichItem = 1
+                                buyTitle = "Diamond Pack 1"
+                                buyText = "400 Diamonds"
+                                confirmDialog = true
+                                withAnimation(.easeInOut(duration: TouhouSiegeStyle.BigDecimals.small)) {
+                                    isAnimating.toggle()
+                                }
+                            }, titel: "Diamond Pack 1", text: "400 Diamonds", cost: "6.99€")
+                            
+                            ShopItemLister(function: {
+                                whichItem = 2
+                                buyTitle = "Diamond Pack 2"
+                                buyText = "900 Diamonds"
+                                confirmDialog = true
+                                withAnimation(.easeInOut(duration: TouhouSiegeStyle.BigDecimals.small)) {
+                                    isAnimating.toggle()
+                                }
+                            }, titel: "Diamond Pack 2", text: "900 Diamonds", cost: "12.99€")
+                            
+                            ShopItemLister(function: {
+                                whichItem = 3
+                                buyTitle = "Diamond Pack 3"
+                                buyText = "2000 Diamonds"
+                                confirmDialog = true
+                                withAnimation(.easeInOut(duration: TouhouSiegeStyle.BigDecimals.small)) {
+                                    isAnimating.toggle()
+                                }
+                            }, titel: "Diamond Pack 3", text: "2000 Diamonds", cost: "24.99€")
+                        }
+                        
+                        if whichTab == 2 {
+                            ShopItemLister(function: {
+                                whichItem = 101
+                                buyTitle = "Stamina Pack"
+                                buyText = "120 Stamina"
+                                confirmDialog = true
+                                withAnimation(.easeInOut(duration: TouhouSiegeStyle.BigDecimals.small)) {
+                                    isAnimating.toggle()
+                                }
+                            }, titel: "Stamina Pack", text: "120 Stamina", cost: "750", image: Image(TouhouSiegeStyle.Images.gold01))
+                            
+                            ShopItemLister(function: {
+                                whichItem = 102
+                                buyTitle = "Gold Pack 1"
+                                buyText = "400 Gold"
+                                confirmDialog = true
+                                withAnimation(.easeInOut(duration: TouhouSiegeStyle.BigDecimals.small)) {
+                                    isAnimating.toggle()
+                                }
+                            }, titel: "Gold Pack 1", text: "400 Gold", cost: "100", image: Image(TouhouSiegeStyle.Images.diamond01))
+                            
+                            ShopItemLister(function: {
+                                whichItem = 103
+                                buyTitle = "Gold Pack 1"
+                                buyText = "1000 Gold"
+                                confirmDialog = true
+                                withAnimation(.easeInOut(duration: TouhouSiegeStyle.BigDecimals.small)) {
+                                    isAnimating.toggle()
+                                }
+                            }, titel: "Gold Pack 2", text: "1000 Gold", cost: "200", image: Image(TouhouSiegeStyle.Images.diamond01))
+                        }
+                    }
                 }
+                
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        ButtonBig(function: {
+                            navigationManager.navigateTo(screen: .home)
+                        }, text: "Back").offset(x: width * TouhouSiegeStyle.Decimals.medium, y: -width * TouhouSiegeStyle.Decimals.xSmall)
+                        
+                        Spacer()
+                    }
+                }
+            }.disabled(confirmDialog)
+            
+            if confirmDialog {
+                    ConfirmDialog(functionYes: {
+                        
+                    }, functionNo: {
+                        withAnimation(.easeInOut(duration: 0.3)) {
+                            isAnimating.toggle()
+                        }
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3, execute: {
+                            whichItem = 0
+                            buyTitle = ""
+                            buyText = ""
+                            confirmDialog = false
+                        })
+                        
+                    }, title: "Are you sure about this purchase?", buyTitle: buyTitle, buyText: buyText)
+                    .opacity(isAnimating ? 1 : 0)
             }
         }
     }
